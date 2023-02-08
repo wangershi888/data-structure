80423e692dae4943818d834d7b7e5066866d1bd1
王贰拾 <wangwenkai5052@outlook.com>
2023-02-09 00:12:07 +0800

    feat: 字典

--- 5、字典.js
+++ 5、字典.js
@@ -1,1 +1,24 @@
-class Map {}
+class Dictionary {
+  items = {};
+  has(key) {
+    // return this.items.hasOwnProperty(key);
+    return key in this.items;
+  }
+  set(key, value) {
+    this.items[key] = value;
+  }
+  delete(key) {
+    return this.items.has(key) ? delete this.items[key] : false;
+  }
+  get(key) {
+    return this.items[key];
+  }
+  getItems() {
+    return this.items;
+  }
+}
+
+let a = new Dictionary();
+a.set("a", "a");
+a.set("b", "b");
+console.log(a.getItems());
